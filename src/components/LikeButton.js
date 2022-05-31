f851164b3efc92ea97c2b2da06738490da31fee8
Will Braun <willbraun@Wills-MacBook-Pro.local>
2022-05-31 14:24:50 -0400

    updated text and styling of button

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -2,13 +2,26 @@ import { useState } from 'react';
 
 const LikeButton = () => {
     const [count, setCount] = useState(0);
+    const [likeWord, setLikeWord] = useState('Likes');
 
-    const updateCount = () => {
+    const updateButtonText = () => {
         setCount(count + 1);
+        if (count > 1) {
+            return;
+        }
+        else if (count === 0) {
+            setLikeWord('Like');
+            return;
+        }
+        else if (count === 1) {
+            setLikeWord('Likes');
+        }
     }
 
     return (
-        <button type="button" onClick={updateCount}>{count} Likes</button>
+        <button type="button" onClick={updateButtonText}>
+            <p>{count} {likeWord}</p>
+        </button>
     );
 }
 
